6483f7c2b71457bfe2164a80f30d7419afdc0f1d
Juan Carlos Kuri Pinto <jckuri@gmail.com>
2021-03-28 15:36:06 -0500

    Add files via upload

---
+++ copy_files.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/udacity/nd013-c6-control-starter.git
+cp code/compile_pid_controller.sh nd013-c6-control-starter/project/
+cp code/simulatorAPI.py nd013-c6-control-starter/project/
+cp code/plot_pid_info.py nd013-c6-control-starter/project/
+cp code/main.cpp nd013-c6-control-starter/project/pid_controller/
+cp code/pid_controller.cpp nd013-c6-control-starter/project/pid_controller/
+cp code/pid_controller.h nd013-c6-control-starter/project/pid_controller/
